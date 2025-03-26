ca2edd946ae010a020a2dff2849130edbcf83061
HawraaYounes <hawraayounes0@gmail.com>
2025-03-26 22:11:37 +0200

    added input component

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,24 @@
+
+const Input = ({ label, name, placeholder, type = "text", width = "w-full" }) => {
+    return (
+      <div className={`flex flex-col items-start ${width}`}>
+        <label
+          htmlFor={name}
+          className="block mb-[22px] font-medium font-poppins"
+        >
+          {label}
+        </label>
+        <input
+          id={name}
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          className="bg-white border border-gray mb-3 py-[25px] px-[30px] rounded-[10px] focus:outline-none min-w-[400px] max-w-[500px]"
+          required
+        />
+      </div>
+    );
+  };
+  
+  export default Input;
+  
