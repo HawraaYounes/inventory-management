39bd7157b35cfb7652106ed8b2f719d0b782d8ba
HawraaYounes <hawraayounes0@gmail.com>
2025-03-26 20:15:35 +0200

    updated routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-import AppRouter from "./routes/Routes";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Auth />,
+  
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 function App() {
-  return <AppRouter />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
