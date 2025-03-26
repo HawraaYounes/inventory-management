13ebc754f13be24bd3461916967912ef9e50b78a
HawraaYounes <hawraayounes0@gmail.com>
2025-03-26 22:09:43 +0200

    added auth form to auth page

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useActionData } from "react-router-dom";
 import { ALERT_TYPES } from "../constants";
-import { successIcon } from "../assets";
+import AuthForm from "../components/AuthForm";
+import { json, redirect } from "react-router-dom";
+import Alert from "../components/Alert";
 
 
 const Auth = () => {
@@ -24,69 +26,76 @@ const Auth = () => {
   }, [actionData]);
 
   return (
-    <>
-     Hi
-    </>
+    <div className="items-center">
+       {flashMessage.message && (
+        <Alert
+          type={flashMessage.type}
+          message={flashMessage.message}
+          description={flashMessage.description}
+        />
+      )}
+      <AuthForm setFlashMessage={setFlashMessage} />
+    </div>
   );
 };
 
 export default Auth;
 
-// export async function action({ request }) {
-//   const data = await request.formData();
-//   const searchParams = new URL(request.url).searchParams;
-//   const mode = searchParams.get("mode") || "signup";
+export async function action({ request }) {
+  const data = await request.formData();
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "signup";
 
-//   if (mode !== "login" && mode !== "signup") {
-//     return json({ message: "Invalid mode!" }, { status: 422 });
-//   }
+  if (mode !== "login" && mode !== "signup") {
+    return json({ message: "Invalid mode!" }, { status: 422 });
+  }
 
-//   const authData = {
-//     email: data.get("email"),
-//     password: data.get("password"),
-//   };
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
 
-//   if (mode === "signup") {
-//     authData.name = data.get("name");
-//     authData.password_confirmation = data.get("confirmPassword");
+  if (mode === "signup") {
+    authData.name = data.get("name");
+    authData.password_confirmation = data.get("confirmPassword");
 
-//     if (authData.password !== authData.password_confirmation) {
-//       return json(
-//         {
-//           message: "Oops!",
-//           type: ALERT_TYPES.WARNING,
-//           description: "Passwords do not match.",
-//         },
-//         { status: 422 }
-//       );
-//     }
-//   }
+    if (authData.password !== authData.password_confirmation) {
+      return json(
+        {
+          message: "Oops!",
+          type: ALERT_TYPES.WARNING,
+          description: "Passwords do not match.",
+        },
+        { status: 422 }
+      );
+    }
+  }
 
-//   try {
-//     const response = await axios.post(`${API_BASE_URL}/api/${mode}`, authData, {
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       withCredentials: true,
-//     });
+  try {
+    const response = await axios.post(`${API_BASE_URL}/api/${mode}`, authData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+    });
 
-//     localStorage.setItem("token", response.data.token);
+    localStorage.setItem("token", response.data.token);
 
-//     return redirect("/dashboard");
-//   } catch (error) {
-//     const errorMessage =
-//       error.response?.data?.message ||
-//       (error.response?.data?.errors
-//         ? Object.values(error.response.data.errors).join(" ")
-//         : "An unexpected error occurred!");
+    return redirect("/dashboard");
+  } catch (error) {
+    const errorMessage =
+      error.response?.data?.message ||
+      (error.response?.data?.errors
+        ? Object.values(error.response.data.errors).join(" ")
+        : "An unexpected error occurred!");
 
-//     return json(
-//       {
-//         message: "Oops!",
-//         description: errorMessage,
-//         type: ALERT_TYPES.WARNING,
-//       },
-//       { status: 400 }
-//     );
-//   }
-// }
+    return json(
+      {
+        message: "Oops!",
+        description: errorMessage,
+        type: ALERT_TYPES.WARNING,
+      },
+      { status: 400 }
+    );
+  }
+}
