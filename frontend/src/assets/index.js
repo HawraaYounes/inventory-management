f15d680108b9a6014640b56fda7fa3218d1edb8a
HawraaYounes <hawraayounes0@gmail.com>
2025-03-29 13:29:48 +0200

    updated alert component

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -3,6 +3,7 @@ import questionIcon from "./icons/question.svg";
 import successIcon from "./icons/success.svg";
 import warningIcon from "./icons/warning.svg";
 import failIcon from "./icons/fail.svg";
+import closeIcon from "./icons/close.svg";
 
 import greenBubbles from "./images/green-bubbles.png";
 import redBubbles from "./images/red-bubbles.png";
@@ -14,6 +15,7 @@ export {
   successIcon,
   warningIcon,
   failIcon,
+  closeIcon,
   greenBubbles,
   redBubbles,
   grayBubbles,
