7c7ff2839f7ea6fd8f74620f3c4303a0eaef6615
HawraaYounes <hawraayounes0@gmail.com>
2025-04-19 11:21:21 +0300

    updated buttons sizes

--- frontend/src/components/ui/atoms/Button.jsx
+++ frontend/src/components/ui/atoms/Button.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-
 const Button = ({ label, variant = 'hero', ...props }) => {
+  const baseStyles = "font-poppins rounded-md min-w-[150px] text-nowrap";
+
   const buttonStyle =
     variant === 'hero'
-      ? "font-poppins bg-[#B88E2F] text-white py-4 px-11 md:py-[25px] md:px-[72px] md:mt-10 mt-2 font-semibold"
+      ? `${baseStyles} bg-[#B88E2F] text-white py-4 px-11 md:py-[15px] md:px-[50px] font-semibold`
       : variant === 'outline'
-      ? "font-poppins bg-white text-black border border-black py-3 px-8 w-full text-nowrap uto md:py-[15px] md:px-[50px] font-normal rounded-lg  "
-      : "font-poppins bg-white text-[#B88E2F] border-2 border-[#B88E2F] py-3 px-8 md:py-[15px] md:px-[50px] md:mt-5 mt-1 font-semibold";
+      ? `${baseStyles} bg-white text-black border border-black py-4 px-11 md:py-[15px] md:px-[50px] font-normal`
+      : `${baseStyles} bg-white text-[#B88E2F] border-2 border-[#B88E2F] py-4 px-11 md:py-[15px] md:px-[50px] font-semibold`;
 
   return (
-    <button className={`${buttonStyle}`} {...props}>
+    <button className={buttonStyle} {...props}>
       {label}
     </button>
   );
